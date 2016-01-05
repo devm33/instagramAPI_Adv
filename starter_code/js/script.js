43b03c26baf6df6097870ab5d5cd13148c55a04a
Devraj Mehta <devm33@gmail.com>
2016-01-05 02:25:09 +0000

    saving devm33 client id

--- starter_code/js/script.js
+++ starter_code/js/script.js
@@ -1,1 +1,6 @@
-// YOUR CODE GOES HERE
+/*global searchInstagram*/
+searchInstagram('78cf14b4cd764ccf9be980501c4f2c24', 'yolo', 10, onSearchReturn);
+
+function onSearchReturn() {
+    alert(arguments);
+}
